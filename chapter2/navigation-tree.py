29a1bc2d1d3b8833ff715688bc0864b019be9dfa
xiaowang1105 <wx961105@outlook.com>
2017-02-20 23:54:56 +0800

    parent dealing

--- chapter2/navigation-tree.py
+++ chapter2/navigation-tree.py
@@ -19,5 +19,9 @@ for child in bs0bj.find("table",{"id":"giftList"}).descendants:
     print(child)
 """
 
+"""
 for sibling in bs0bj.find("table",{"id":"giftList"}).tr.next_siblings:
     print(sibling)
+"""
+
+print(bs0bj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
